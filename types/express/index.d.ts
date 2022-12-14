27f0f0109040bb46d6c00851aba6baf48f2da1f4
Sebastian Suarez <sebastian.suarez@limberhealth.com>
2022-12-13 19:17:50 -0500

    Update values

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -4,6 +4,5 @@ export declare module "express-serve-static-core" {
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 	interface Request {
 		user: unknown;
-		promise: Promise<unknown>;
 	}
 }
