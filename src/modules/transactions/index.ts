9579506b67bda3ea2faf447cbb30395c51666b85
Sebastian Suarez <apogees-miscue-0t@icloud.com>
2023-03-23 19:10:51 -0500

    Transactions

--- src/modules/transactions/index.ts
+++ src/modules/transactions/index.ts
@@ -1,12 +1,14 @@
 import { Router as router } from "express";
 import type { Request, Response } from "express";
 import requestMiddleware from "../../utils/request-middleware";
+import validateAuth from "../../middlewares/validate-auth";
 
 const routes = router();
 
 routes.get(
 	"/transactions",
-	requestMiddleware(async (_request: Request, response: Response) => {
+	validateAuth,
+	requestMiddleware(async (request: Request, response: Response) => {
 		const transactions = Array.from({ length: 100 }).map((_, index) => ({
 			id: index,
 			date: Date.now(),
